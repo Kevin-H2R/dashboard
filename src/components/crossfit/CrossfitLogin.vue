cff934abd0b7261ae13005d1dc78b330ecf73c27
Kevin <kevin.herrbach@avaent.co.kr>
2022-03-21 10:23:00 +0900

    Makes login as a form

--- src/components/crossfit/CrossfitLogin.vue
+++ src/components/crossfit/CrossfitLogin.vue
@@ -1,34 +1,38 @@
 <template>
-  <v-row v-show="$store.getters.cookie === null">
-    <v-col>
-      <v-row justify="space-between">
-        <v-text-field
-          class="flex-grow-0"
-          v-model="login"
-          label="Login"
-          outlined
-          dense
-          hide-details
-        />
-        <v-text-field
-          class="flex-grow-0"
-          v-model="password"
-          label="Password"
-          type="password"
-          outlined
-          dense
-          hide-details
-        />
-      </v-row>
-      <v-row justify="center">
-        <v-col cols="12" sm="10">
-          <v-btn width="100%" color="primary" @click="loginClick()">
-            Login
-          </v-btn>
+  <v-form @submit.prevent="loginClick()">
+    <v-container>
+      <v-row v-show="$store.getters.cookie === null">
+        <v-col>
+          <v-row justify="space-between">
+            <v-text-field
+              class="flex-grow-0"
+              v-model="login"
+              label="Login"
+              outlined
+              dense
+              hide-details
+            />
+            <v-text-field
+              class="flex-grow-0"
+              v-model="password"
+              label="Password"
+              type="password"
+              outlined
+              dense
+              hide-details
+            />
+          </v-row>
+          <v-row justify="center">
+            <v-col cols="12" sm="10">
+              <v-btn width="100%" type="submit" color="primary" @click="loginClick()">
+                Login
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
-    </v-col>
-  </v-row>
+    </v-container>
+  </v-form>
 </template>
 <script>
 import axios from "axios";
@@ -37,7 +41,10 @@ export default {
   methods: {
     loginClick: function () {
       axios
-        .post("http://" + process.env.VUE_APP_HOST + ":3000/crossfit/login", {login: this.login, password: this.password})
+        .post("http://" + process.env.VUE_APP_HOST + ":3000/crossfit/login", {
+          login: this.login,
+          password: this.password,
+        })
         .then((res) => {
           this.$store.commit("setCookie", res.data.cookie);
           this.$store.commit("setRegistered", res.data.registered);
