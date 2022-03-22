4989e9dec6dd35ec59f8b10014a7a54463938b1f
Kevin <kevin.herrbach@avaent.co.kr>
2022-03-22 11:40:08 +0900

    Adds title to coding cards

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,24 @@
           </v-col>
         </v-row>
         <todo-container class="mb-2" />
+        <v-card>
+          <v-card-title>Coding language (last 7 days)</v-card-title>
+          <v-divider />
+          <v-card-text>
+            <figure><embed src="https://wakatime.com/share/@Kebinou/d370d33f-31fe-44d4-a49f-436834de6b8f.svg"/></figure>
+          </v-card-text>
+        </v-card>
       </v-col>
       <v-col cols="12" sm="6" lg="4">
         <wod-container class="mb-2" />
         <attendance-calendar class="mb-2" />
+        <v-card>
+          <v-card-title>Coding activity (last 7 days)</v-card-title>
+          <v-divider />
+          <v-card-text>
+            <figure><embed src="https://wakatime.com/share/@Kebinou/d0f1d797-681b-4cc1-a9ed-d0dafc7bbda1.svg"/></figure>
+          </v-card-text>
+        </v-card>
       </v-col>
       <v-col cols="12" sm="6" lg="4">
         <result-container class="mb-2" />
